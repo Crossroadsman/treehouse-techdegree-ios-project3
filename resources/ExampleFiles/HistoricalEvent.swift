ef9b4a9cfaa3fe042ed9a89ba057de1b144cfd21
Alex Koumparos <crossroadsman@gmail.com>
2017-08-25 22:47:10 -0600

    Create HistoricalEvent.swift

---
+++ resources/ExampleFiles/HistoricalEvent.swift
@@ -0,0 +1,29 @@
+struct HistoricalEvent: CustomStringConvertible {
+    
+    public var description: String {
+        return "\(name)"
+    }
+    
+    private let date: Date
+    private let name: String
+    private let url: URL
+    
+    init(name: String, date: Date, url: URL) {
+        self.date = date
+        self.name = name
+        self.url = url
+    }
+    
+    public func getDate() -> Date {
+        return date
+    }
+    
+    public func getName() -> String {
+        return name
+    }
+    
+    public func getUrl() -> URL {
+        return url
+    }
+    
+}
